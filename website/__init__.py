88e73363c6c9398dfeaa33781e644baf61070b78
Alicia Román <135585203+romanalicia@users.noreply.github.com>
2025-05-08 12:11:34 +1200

    create database

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
+db=SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
-
+    app.config['SECRET_KEY'] = "/9n)>RK_,l$mKxN|C.w(!C4>]@poT~g"
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from .views import views
     from .auth import auth 
@@ -11,5 +17,9 @@ def create_app():
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
 
+    from .models import User 
+
+    with app.app_context():
+        db.create_all()
 
     return app
--- website/auth.py
+++ website/auth.py
@@ -3,8 +3,9 @@ from flask import Blueprint, render_template
 auth = Blueprint("auth", __name__)
 
 
-@auth.route("/sign-up")
+@auth.route("/sign-up", methods=['GET','POST'])
 def sign_up(): 
+    
     return render_template("sign_up.html")
 
 @auth.route("/login")
