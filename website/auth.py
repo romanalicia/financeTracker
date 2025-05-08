51017ebc65ba0f7e2cb56ca65537eae9121d6912
Alicia Román <135585203+romanalicia@users.noreply.github.com>
2025-05-08 12:45:47 +1200

    flash messages sign-up form

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, flash, redirect, url_for
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_user, logout_user, login_required, current_user 
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -10,8 +10,36 @@ auth = Blueprint("auth", __name__)
 
 
 @auth.route("/sign-up", methods=['GET','POST'])
-def sign_up(): 
-    
+def sign_up():
+    if request.method == 'POST':
+        email = request.form.get("email")
+        username = request.form.get("username")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+
+    email_exists = User.query.filter_by(email=email).first()
+    username_exists = User.query.filter_by(username=username).first()
+
+    if email_exists: 
+        flash('Email is already in use.', category='error')
+    elif username_exists: 
+        flash('Username is already in use.', category='error')
+    elif password1 != password2: 
+        flash('Passwords do not match.', category='error')
+    elif len(password1) < 8: 
+        flash('Password is too short.', category='error')
+    elif len(username) < 2: 
+        flash('Username is too short.', category='error')
+    elif len(email) < 4: 
+        flash('Email is not valid.', category='error')
+    else: 
+        new_user = User(email=email, username=username, password=generate_password_hash(password1, method='scrypt:32768:8:1'))
+        db.session.add(new_user)
+        db.session.commit()
+        login_user(new_user, remember=True)
+        flash('Ypur account has been created!', category='success')
+        return redirect(url_for('views.home'))
+
     return render_template("sign_up.html")
 
 @auth.route("/login")
