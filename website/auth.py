aed80f01842e44e0f7b6c9e6d50938e4f071d820
Alicia Román <135585203+romanalicia@users.noreply.github.com>
2025-06-04 09:20:52 +1200

    fixing database

--- website/auth.py
+++ website/auth.py
@@ -9,43 +9,78 @@ from .models import User
 auth = Blueprint("auth", __name__)
 
 
+# sign-up route
 @auth.route("/sign-up", methods=['GET','POST'])
-def sign_up():
+# sign up function
+# returns sign up page
+def sign_up():  
+    # signup function
+    # returns signup page
     if request.method == 'POST':
         email = request.form.get("email")
         username = request.form.get("username")
         password1 = request.form.get("password1")
         password2 = request.form.get("password2")
-
+        # check that the email and username are unique
         email_exists = User.query.filter_by(email=email).first()
         username_exists = User.query.filter_by(username=username).first()
-
-        if email_exists: 
-            flash('Email is already in use.', category='error')
-        elif username_exists: 
-            flash('Username is already in use.', category='error')
-        elif password1 != password2: 
-            flash('Passwords do not match.', category='error')
-        elif len(password1) < 8: 
-            flash('Password is too short.', category='error')
-        elif len(username) < 2: 
-            flash('Username is too short.', category='error')
-        elif len(email) < 4: 
-            flash('Email is not valid.', category='error')
-        else: 
+        # validation of password, username and email
+        if email_exists:
+            flash('Email is already in use.' , category='error')
+        elif username_exists:
+            flash('Username is already in use.' , category='error')
+        elif password1 != password2:
+            flash('Passwords do not match!' , category='error')
+        elif len(password1) < 8:
+            flash('Password is too short.' , category='error')
+        elif len(username) < 2:
+            flash('Username is too short.' , category='error')
+        elif len(email) < 4:
+            flash('Email is not valid.' , category='error')
+        else:
             new_user = User(email=email, username=username, password=generate_password_hash(password1, method='scrypt:32768:8:1'))
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user, remember=True)
-            flash('Your account has been created!', category='success')
+            flash('Your accout has been created!', category= 'success')
             return redirect(url_for('views.home'))
+        
+    return render_template("sign_up.html", user=current_user)
 
-    return render_template("sign_up.html")
 
-@auth.route("/login")
-def login(): 
-    return render_template("login.html")
+# login route
+@auth.route("/login", methods=['GET','POST'])
+# login function
+# returns login page
+def login():
+    # gets email and password from login form
+    if request.method == 'POST':
+        email = request.form.get("email")
+        password = request.form.get("password")
+        # queries database to recieve user information using email address
+        user = User.query.filter_by(email=email).first()
+        # checks email and password
+        if user:
+            # if correct log in user and redirect to home page
+            if check_password_hash(user.password, password):
+                flash("Logged In!", category="success")
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            # if incorrect password flash error
+            else:
+                flash("Incorrect Password!", category="error")
+        # if incorrect email flash error
+        else:
+            flash("Email does not exist!", category="error")
 
+
+    return render_template("login.html", user=current_user)
+
+# logout route
 @auth.route("/logout")
-def logout(): 
-    return render_template("logout.html")
+@login_required
+# logout function
+# returns logout page
+def logout():
+    logout_user()
+    return redirect(url_for('views.home'))
