62cf8062b6f7d8a6b012e9c512c5a451f7a99b27
Alicia Román <135585203+romanalicia@users.noreply.github.com>
2025-07-17 12:53:16 +1200

    create goals and savings page

--- website/views.py
+++ website/views.py
@@ -1,16 +1,22 @@
 from flask import Blueprint, render_template
-from flask_login import login_user, logout_user, login_required, current_user 
+from flask_login import login_user, logout_user, login_required, current_user
 
 views = Blueprint("views", __name__)
 
 
 @views.route("/")
 @views.route("/home")
-def home(): 
+def home():
     return render_template("home.html", user=current_user)
+
 
 @views.route("/track-expenses", methods=['GET', 'POST'])
 @login_required
-def track_expenses(): 
+def track_expenses():
     return render_template("track_expenses.html", user=current_user)
 
+
+@views.route("/goals-and-savings", methods=['GET', 'POST'])
+@login_required
+def goals_and_expenses():
+    return render_template("goals-and-savings.html", user=current_user)
