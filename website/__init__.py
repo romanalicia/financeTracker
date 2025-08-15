0b28e76fb096b29e133098c7bc7b510b0ed89145
Alicia Román <135585203+romanalicia@users.noreply.github.com>
2025-08-15 19:55:45 +1200

    __innit__.py pep8 edits

--- website/__init__.py
+++ website/__init__.py
@@ -1,23 +1,26 @@
-from flask import Flask 
+"""Initiates Flask application."""
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
-db=SQLAlchemy()
+db = SQLAlchemy()
 DB_NAME = "database.db"
 
+
 def create_app():
+    """Create and configure the Flask application."""
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "/9n)>RK_,l$mKxN|C.w(!C4>]@poT~g"
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
     from .views import views
-    from .auth import auth 
+    from .auth import auth
 
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
 
-    from .models import User 
+    from .models import User
 
     with app.app_context():
         db.create_all()
